8ad98888775eb9e1d72047088067d884059edf04
Harshit Jain <harshitjain0502@gmail.com>
2025-01-15 22:03:56 +0530

    Assignment-2

---
+++ A2/child.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <string.h>
+
+const char* PID_TXT_FILE = "childpid.txt";
+const char* DUM_TXT_FILE = "dummycpid.txt";
+enum Status{
+    PLAYING,
+    CATCHMADE,
+    CATCHMISSED,
+    OUTOFGAME
+} currStat;
+pid_t nextpid=-1;
+int ch;
+
+void attemptCatch(int){
+    int num = rand()%10;
+    pid_t ppid = getppid();
+    if(num<=1){
+        currStat = CATCHMISSED;
+        kill(ppid,SIGUSR2);
+    }
+    else{
+        currStat = CATCHMADE;
+        kill(ppid,SIGUSR1);
+    }
+}
+
+void printStatus(int){
+    switch(currStat){
+        case PLAYING:
+            printf("....\t");
+            break;
+        case OUTOFGAME:
+            printf("    \t");
+            break;
+        case CATCHMADE:
+            printf("CATCH\t");
+            currStat = PLAYING;
+            break;
+        case CATCHMISSED:
+            printf("MISS\t");
+            currStat = OUTOFGAME;
+            break;
+    }
+    fflush(stdout);
+
+    if(nextpid>0){
+        kill(nextpid,SIGUSR1);
+    }
+    else{
+        FILE* dfp = fopen(DUM_TXT_FILE,"r");
+        pid_t dpid;
+        fscanf(dfp,"%d",&dpid);
+        fclose(dfp);
+        kill(dpid,SIGINT);
+    }
+}
+
+void exitGame(int){
+    if(currStat==PLAYING){
+        printf("\n+++ Child %d: Yay! I am the winner!\n", ch);
+        fflush(stdout);
+    }
+    exit(0);
+}
+
+int main(int argc, const char* argv[]){
+    if(argc<2){
+        printf("Usage: %s [n]\n",argv[0]);
+        exit(0);
+    }
+    sleep(2);
+    srand((unsigned int)getpid());
+
+    ch = atoi(argv[1]);
+    currStat = PLAYING;
+
+    FILE *fp = fopen(PID_TXT_FILE,"r");
+    int n;
+    fscanf(fp,"%d",&n);
+    for(int j=1;j<=n;j++){
+        fscanf(fp,"%d",&nextpid);
+        if(j==ch%n+1) break;
+    }
+    if(ch==n) nextpid=-1;
+
+    // printf("%d %d %d\n",ch, getpid(),nextpid);
+    fclose(fp);
+
+    signal(SIGUSR1,printStatus);
+    signal(SIGUSR2,attemptCatch);
+    signal(SIGINT,exitGame);
+
+    while(1){
+        pause();
+    }
+}
---
+++ A2/parent.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <string.h>
+#include <stdbool.h>
+
+const char* PID_TXT_FILE = "childpid.txt";
+const char* DUM_TXT_FILE = "dummycpid.txt";
+const char* CHILD = "./child";
+const char* DUMMY = "./dummy";
+
+bool* isPlaying = NULL;
+pid_t* childPID = NULL;
+int n;
+volatile sig_atomic_t caught;
+
+void sigHandler (int sig){
+    switch(sig){
+        case SIGUSR1:
+            caught=1;
+            break;
+        case SIGUSR2:
+            caught=0;
+            break;
+    }
+}
+
+int next(int i){
+    int j = (i+1)%n;
+    for(;!isPlaying[j];j=(j+1)%n);
+    return j;
+}
+
+void printNums(){
+    printf("\n\t");
+    for(int j=1;j<=n;j++) printf("%3d\t",j);
+    fflush(stdout);
+}
+
+void printLine(){
+    printf("\n+");
+    for(int j=0;j<8*(n+1);j++) printf("-");
+    printf("+");
+    fflush(stdout);
+}
+int main(int argc, const char* argv[]){
+    if(argc<2){
+        printf("Usage: %s [n]\n",argv[0]);
+        exit(0);
+    }
+
+    n = atoi(argv[1]);
+    isPlaying = (bool*)malloc(n*sizeof(bool));
+    childPID = (pid_t*)malloc(n*sizeof(pid_t));
+
+    FILE *fp = fopen(PID_TXT_FILE,"w");
+    fprintf(fp,"%d\n",n);
+    fflush(fp);
+
+    for(int i=0;i<n;i++){
+        isPlaying[i]=true;
+        childPID[i] = fork();
+
+        if(childPID[i]){
+            fprintf(fp,"%d\n",childPID[i]);
+            fflush(fp);
+        }
+        else{
+            char num[20];
+            sprintf(num,"%d",i+1);
+            execlp(CHILD,CHILD,num,NULL);
+        }
+    }
+
+    printf("Parent: %d child processes created\n",n);
+    fclose(fp);
+    int t = 3;
+    printf("Parent: Waiting %d seconds for child processes to read child database\n",t);
+    sleep(t);
+
+    printNums();
+    printLine();
+    signal(SIGUSR1,sigHandler);
+    signal(SIGUSR2,sigHandler);
+
+    int ct=n;
+    int curr=-1;
+    while(ct>1){
+        curr = next(curr);
+        kill(childPID[curr],SIGUSR2);
+        pause();
+        if(caught==0){
+            // printf("%d\n",curr);
+            fflush(stdout);
+            isPlaying[curr]=false;
+            ct--;
+        }
+        pid_t dpid = fork();
+        if(dpid){
+            FILE* dfp = fopen(DUM_TXT_FILE,"w");
+            fprintf(dfp,"%d",dpid);
+            fflush(dfp);
+            fclose(dfp);
+            printf("\n|\t");
+            fflush(stdout);
+            kill(childPID[0],SIGUSR1);
+            waitpid(dpid,NULL,0);
+            printf(" |");
+            printLine();
+        }
+        else{
+            execlp(DUMMY,DUMMY,NULL);
+        }
+    }
+
+    printNums();
+
+    for(int i=0;i<n;i++) kill(childPID[i],SIGINT);
+}
